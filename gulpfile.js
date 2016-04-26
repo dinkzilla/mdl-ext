6f135d39f6415626da047f61ed7d2e9ebe91d4ad
Leif Oddvar Olsen <LeifOddvar.Olsen@skatteetaten.no>
2016-04-26 15:00:34 +0200

    build(demo) improved demo build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-const posthtml = require('gulp-posthtml');
 const gulp = require('gulp');
+const posthtml = require('gulp-posthtml');
 
-gulp.task('build-demo', function() {
+gulp.task('build-demo', ['posthtml-build-demo', 'copy-css', 'copy-scripts']);
+
+gulp.task('posthtml-build-demo', function() {
   const plugins = [
     require('posthtml-include')({
       encoding: 'utf-8',
@@ -12,4 +14,14 @@ gulp.task('build-demo', function() {
   return gulp.src('./src/demo/*.html')
     .pipe(posthtml(plugins))
     .pipe(gulp.dest('./demo'));
+});
+
+gulp.task('copy-css', function() {
+  return gulp.src(['./src/demo/styles/*.css', './node_modules/dialog-polyfill/dialog-polyfill.css'])
+    .pipe(gulp.dest('./demo/styles'));
+});
+
+gulp.task('copy-scripts', function() {
+  return gulp.src(['./src/demo/scripts/*.js', './node_modules/dialog-polyfill/dialog-polyfill.js'])
+    .pipe(gulp.dest('./demo/scripts'));
 });
