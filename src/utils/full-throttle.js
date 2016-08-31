c1eb1a89d43acbe106ba7956ef83327c2e6cb899
Leif Olsen <leif.o.olsen@gmail.com>
2016-08-31 16:20:16 +0200

    refactor(throttle): Added a simplified throttle function which throttles @60 fps

---
+++ src/utils/full-throttle.js
@@ -0,0 +1,30 @@
+/**
+ * Since some events can fire at a high rate, the event handler should be limited to execute computationally
+ * expensive operations, such as DOM modifications, inside a single rendered frame.
+ * When listening to e.g. scroll and resize events, the browser tends to fire off more events per
+ * second than are actually useful. For instance, if your event listener sets some element positions, then it
+ * is possible for those positions to be updated multiple times in a single rendered frame. In this case, all of
+ * the layout calculations triggered by setting the elements' positions will be wasted except for the one time that
+ * it runs immediately prior to the browser rendering the updated layout to the screen.
+ * To avoid wasting cycles, we can use requestAnimationFrame to only run the event listener once just before the page
+ * is rendered to the screen.
+ * *
+ * @param callback the function to throttle
+ * @return {function(...[*])}
+ */
+const fullThrottle = (callback) => {
+
+  let throttling = false;
+
+  return (...args) => {
+    if(!throttling) {
+      throttling = true;
+      window.requestAnimationFrame( () => {
+        callback(...args);
+        throttling = false;
+      });
+    }
+  };
+};
+
+export default fullThrottle;
